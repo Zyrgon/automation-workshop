bc045348d18a5fb2ffd65ebe812804daf68762eb
Tomáš Drlíček <tomas.drlicek@telia.no>
2023-10-18 14:24:07 +0200

    Spolecna prace examples, name fixture added

--- cypress/e2e/spolecne.cy.js
+++ cypress/e2e/spolecne.cy.js
@@ -4,6 +4,58 @@ let searchString = 'Testing'
 
 describe('My first test', () => {
 
+    it('Automation exercise Search products via form', () => {
+        cy.request(
+            {
+                method: 'POST',
+                url: 'https://automationexercise.com/api/searchProduct',
+                form: true,
+                body: 'search_product=top'
+            }
+        ).should((response) => {
+            expect(response.status).to.equal(200)
+            //Parse ugly JSON string into understandable json
+            const json = JSON.parse(response.body);
+            //Asserting first item (this is enough)
+            console.log(json)
+            expect(json.products[0].category.category).to.equal("Tops")
+            //Asserting all items, easy style
+            for(let i = 0; i < json.products.length; i++){
+                //Yes, the endpoint returns something else than tops and should fail!
+                expect(json.products[i].category.category).to.contain("Tops")
+            }
+        })
+    })
+
+    it('Try login as an existing user', () => {
+        let userEmail = 'test1@example.com'
+
+        cy.request(
+            {
+                method: 'POST',
+                url: 'https://automationexercise.com/api/verifyLogin',
+                form: true,
+                body: {email: userEmail , password: 'test123'}
+            }
+        ).should((response) => {
+            expect(response.status).to.equal(200)
+            const json = JSON.parse(response.body);
+            expect(json.message).to.equal("User exists!")
+        })
+
+        cy.request(
+            {
+                method: 'POST',
+                url: 'https://automationexercise.com/api/getUserDetailByEmail?email=' + userEmail
+            }
+        ).should((response) => {
+            expect(response.status).to.equal(200)
+            const json = JSON.parse(response.body);
+            expect(json.message).to.equal("User exists!")
+        })
+    })
+
+
     xit('Create pet', () => {
         cy.request('POST', 'https://petstore.swagger.io/v2/pet', {
             "id": 15,
